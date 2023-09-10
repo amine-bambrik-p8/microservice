b9a15a64dd91792ec65081d31202981d80ebeaac
Amine Bambrik <amine.bambrik@weareams.com>
2023-09-10 11:58:41 +0200

    EC-5: setup order-service api

--- apps/order-service/src/app/app.module.ts
+++ apps/order-service/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { serviceProviders } from './services';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  providers: [...serviceProviders],
 })
 export class AppModule {}
--- apps/order-service/src/main.ts
+++ apps/order-service/src/main.ts
@@ -3,20 +3,24 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+import { IAppConfigService } from './app';
+async function setupApi(app: INestApplication) {
+  const appConfig = app.get(IAppConfigService);
+  const globalPrefix = appConfig.getApiPrefix();
+  const port = appConfig.getApiPort();
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+}
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await setupApi(app);
 }
 
 bootstrap();
